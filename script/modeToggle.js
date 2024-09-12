ee8fe7954656f6a2d534d3a548bdb3060757e34c
KevinPratamaSetiawan <127008007+KevinPratamaSetiawan@users.noreply.github.com>
2024-09-12 17:17:00 +0700

    Theme and Font Update

--- script/modeToggle.js
+++ script/modeToggle.js
@@ -1,40 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('mode-btn');
-    const toggleButtonLogo = document.getElementById('mode-icon');
-    const logo = document.getElementById('mode-logo');
+const toggleButton = document.getElementById('mode-btn');
+const toggleButtonLogo = document.getElementById('mode-icon');
+const logo = document.getElementById('mode-logo');
 
+document.addEventListener('DOMContentLoaded', () => {
     const applyMode = () => {
         const savedMode = localStorage.getItem('currentMode');
+        const headerFontFamily = localStorage.getItem('currentHeaderFont');
+        const textFontFamily = localStorage.getItem('currentTextFont');
+
+        if(document.getElementById('themePopover')){
+            document.getElementById('heading-display').innerHTML = 'Heading: ' + headerFontFamily;
+            document.getElementById('text-display').innerHTML = 'Normal Text: ' + textFontFamily;
+        }
+
+        localStorage.setItem('currentHeaderFont', headerFontFamily);
+        localStorage.setItem('currentTextFont', textFontFamily);
 
         if (savedMode){
-            if (savedMode === 'light') {
-                if(toggleButtonLogo){
-                    toggleButtonLogo.classList.add('fa-sun')
-                    toggleButtonLogo.classList.remove('fa-cloud-moon')
-                }
-                logo.src = '/black-logo.png';
-                document.documentElement.style.setProperty('--background-color', '#fdfdfd');
-                document.documentElement.style.setProperty('--background-color-rgb', '253,253,253');
-                document.documentElement.style.setProperty('--text-color', '#23272d');
-                document.documentElement.style.setProperty('--copy-color', '#23272d');
-                document.documentElement.style.setProperty('--projectbox', '#c3e1c9');
-                document.documentElement.style.setProperty('--projectbox-text', '#1e2126');
-            }else if (savedMode === 'dark') {
-                if(toggleButtonLogo){
-                    toggleButtonLogo.classList.add('fa-moon')
-                    toggleButtonLogo.classList.remove('fa-sun')
-                }
-                logo.src = '/white-logo.png';
-                document.documentElement.style.setProperty('--background-color', '#23272d');
-                document.documentElement.style.setProperty('--background-color-rgb', '35,39,45');
-                document.documentElement.style.setProperty('--text-color', '#fdfdfd');
-                document.documentElement.style.setProperty('--copy-color', '#fdfdfd');
-                document.documentElement.style.setProperty('--projectbox', '#1e2126');
-                document.documentElement.style.setProperty('--projectbox-text', '#c3e1c9');
-            }
-
+            changeMode(savedMode);
         }else {
             localStorage.setItem('currentMode', 'light');
+
+            // Save Default Light Mode
+            localStorage.setItem('currentLightBgColor', '#fdfdfd');
+            localStorage.setItem('currentLightBgColorHsl', '253,253,253');
+            localStorage.setItem('currentLightTextColor', '#23272d');
+            localStorage.setItem('currentLightAccentColor', '#60c17d');
+
+            // Save Default Dark Mode
+            localStorage.setItem('currentDarkBgColor', '#23272d');
+            localStorage.setItem('currentDarkBgColorHsl', '253,253,253');
+            localStorage.setItem('currentDarkTextColor', '#fdfdfd');
+            localStorage.setItem('currentDarkAccentColor', '#c16560');
         }
     };
 
@@ -47,70 +44,124 @@ document.addEventListener('DOMContentLoaded', () => {
             savedMode = 'light';
         }
 
-        if (savedMode === 'light') {
-            savedMode = 'dark';
-        }else if (savedMode === 'dark') {
-            savedMode = 'light';
-        }
+        let modes = ['light', 'dark'];
+        let currentIndex = modes.indexOf(savedMode);
+
+        savedMode = modes[(currentIndex + 1) % modes.length];
     
-        if (savedMode === 'light') {
-            if(toggleButtonLogo){
-                toggleButtonLogo.classList.add('fa-sun')
-                toggleButtonLogo.classList.remove('fa-moon')
-            }
-            logo.src = '/black-logo.png';
-            document.documentElement.style.setProperty('--background-color', '#fdfdfd');
-            document.documentElement.style.setProperty('--background-color-rgb', '253,253,253');
-            document.documentElement.style.setProperty('--text-color', '#23272d');
-            document.documentElement.style.setProperty('--copy-color', '#23272d');
-            document.documentElement.style.setProperty('--projectbox', '#c3e1c9');
-            document.documentElement.style.setProperty('--projectbox-text', '#1e2126');
-        }else if (savedMode === 'dark') {
-            if(toggleButtonLogo){
-                toggleButtonLogo.classList.add('fa-moon')
-                toggleButtonLogo.classList.remove('fa-sun')
-            }
-            logo.src = '/white-logo.png';
-            document.documentElement.style.setProperty('--background-color', '#23272d');
-            document.documentElement.style.setProperty('--background-color-rgb', '35,39,45');
-            document.documentElement.style.setProperty('--text-color', '#fdfdfd');
-            document.documentElement.style.setProperty('--copy-color', '#fdfdfd');
-            document.documentElement.style.setProperty('--projectbox', '#1e2126');
-            document.documentElement.style.setProperty('--projectbox-text', '#c3e1c9');
-        }
+        changeMode(savedMode);
 
         localStorage.setItem('currentMode', savedMode);
     });
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const accentButton = document.getElementById('accent-btn');
+const accentButton = document.getElementById('accent-btn');
 
-    const applyMode = () => {
-        const savedBrandColor = localStorage.getItem('brand-color');
-        if (savedBrandColor) {
-            document.documentElement.style.setProperty('--brand-color', savedBrandColor);
-        }
-    };
+accentButton.addEventListener('click', () => {
+    const savedMode = localStorage.getItem('currentMode');
+    const colors = [
+        '#60c17d', '#c16560', '#336699', '#e7ab2a',
+        '#f25f4c', '#2cb67d', '#A8D672', '#339988',
+        '#ddd05b', '#FF4500', '#00CED1', '#FFD700',
+        '#FF69B4', '#7FFF00', '#C96868', '#FADFA1',
+        '#7EACB5', '#697565'
+    ];
 
-    applyMode();
+    const randomIndex = Math.floor(Math.random() * colors.length);
 
-    accentButton.addEventListener('click', () => {
-        //Brand Color Changer
-        const colors = [
-            '#60c17d', '#c16560', '#336699', '#e7ab2a',
-            '#f25f4c', '#2cb67d', '#A8D672', '#339988',
-            '#ddd05b', '#FF4500', '#00CED1', '#FFD700',
-            '#FF69B4', '#7FFF00', '#C96868', '#FADFA1',
-            '#7EACB5', '#697565'
-        ];
-    
-        const randomIndex = Math.floor(Math.random() * colors.length);
-    
-        const newColor = colors[randomIndex];
-    
-        document.documentElement.style.setProperty('--brand-color', newColor);
+    const newColor = colors[randomIndex];
 
-        localStorage.setItem('brand-color', newColor);
-    });
+    document.documentElement.style.setProperty('--brand-color', newColor);
+
+    if (savedMode === 'light'){
+        localStorage.setItem('currentLightAccentColor', newColor);
+    }else if (savedMode === 'dark'){
+        localStorage.setItem('currentDarkAccentColor', newColor);
+    }
 });
+
+
+function changeMode (savedMode) {
+    if (savedMode === 'light') {
+        if(toggleButtonLogo){
+            toggleButtonLogo.classList.add('fa-sun')
+            toggleButtonLogo.classList.remove('fa-moon')
+        }
+        logo.src = '/black-logo.png';
+
+        const bgColor = localStorage.getItem('currentLightBgColor');
+        const bgColorHsl = localStorage.getItem('currentLightBgColorHsl');
+        const textColor = localStorage.getItem('currentLightTextColor');
+        const accentColor = localStorage.getItem('currentLightAccentColor');
+
+        document.documentElement.style.setProperty('--background-color', bgColor);
+        document.documentElement.style.setProperty('--background-color-rgb', bgColorHsl);
+        document.documentElement.style.setProperty('--text-color', textColor);
+        document.documentElement.style.setProperty('--brand-color', accentColor);
+    }else if (savedMode === 'dark') {
+        if(toggleButtonLogo){
+            toggleButtonLogo.classList.add('fa-moon')
+            toggleButtonLogo.classList.remove('fa-sun')
+        }
+        logo.src = '/white-logo.png';
+        
+        const bgColor = localStorage.getItem('currentDarkBgColor');
+        const bgColorHsl = localStorage.getItem('currentDarkBgColorHsl');
+        const textColor = localStorage.getItem('currentDarkTextColor');
+        const accentColor = localStorage.getItem('currentDarkAccentColor');
+        
+        document.documentElement.style.setProperty('--background-color', bgColor);
+        document.documentElement.style.setProperty('--background-color-rgb', bgColorHsl);
+        document.documentElement.style.setProperty('--text-color', textColor);
+        document.documentElement.style.setProperty('--brand-color', accentColor);
+    }
+}
+
+document.querySelectorAll('.fontItem').forEach(function(button) {
+  button.addEventListener('click', function() {
+      let headerFontFamily = button.getAttribute('data-headerFontFamily');
+      let textFontFamily = button.getAttribute('data-textFontFamily');
+
+      document.getElementById('heading-display').innerHTML = 'Heading: ' + headerFontFamily;
+      document.getElementById('text-display').innerHTML = 'Normal Text: ' + textFontFamily;
+
+      document.documentElement.style.setProperty('--head-font-family', headerFontFamily);
+      document.documentElement.style.setProperty('--base-font-family', textFontFamily);
+
+      localStorage.setItem('currentHeaderFont', headerFontFamily);
+      localStorage.setItem('currentTextFont', textFontFamily);
+  });
+});
+
+document.querySelectorAll('.themeItem').forEach(function(button) {
+    button.addEventListener('click', function() {
+        let mode = button.getAttribute('data-mode');
+        let bgColor = button.getAttribute('data-bgColor');
+        let bgColorHsl = button.getAttribute('data-bgColorRgb');
+        let textColor = button.getAttribute('data-textColor');
+        let accentColor = button.getAttribute('data-accentColor');
+  
+        document.getElementById('bg-display-color').style.backgroundColor = bgColor;
+        document.getElementById('text-display-color').style.backgroundColor = textColor;
+        document.getElementById('accent-display-color').style.backgroundColor = accentColor;
+  
+        document.documentElement.style.setProperty('--background-color', bgColor);
+        document.documentElement.style.setProperty('--background-color-rgb', bgColor);
+        document.documentElement.style.setProperty('--text-color', textColor);
+        document.documentElement.style.setProperty('--brand-color', accentColor);
+
+        if (mode === 'light'){
+            localStorage.setItem('currentMode', mode);
+            localStorage.setItem('currentLightBgColor', bgColor);
+            localStorage.setItem('currentLightBgColorHsl', bgColorHsl);
+            localStorage.setItem('currentLightTextColor', textColor);
+            localStorage.setItem('currentLightAccentColor', accentColor);
+        }else if (mode === 'dark'){
+            localStorage.setItem('currentMode', mode);
+            localStorage.setItem('currentDarkBgColor', bgColor);
+            localStorage.setItem('currentDarkBgColorHsl', bgColorHsl);
+            localStorage.setItem('currentDarkTextColor', textColor);
+            localStorage.setItem('currentDarkAccentColor', accentColor);
+        }
+    });
+  });
